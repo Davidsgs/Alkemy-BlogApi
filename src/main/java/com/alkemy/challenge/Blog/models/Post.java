68cbb12fffbe6c1248aa5276dd256002e8890358
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-06-19 14:39:05 -0300

    Updated the delete method to a Soft Delete.

--- src/main/java/com/alkemy/challenge/Blog/models/Post.java
+++ src/main/java/com/alkemy/challenge/Blog/models/Post.java
@@ -1,12 +1,17 @@
 package com.alkemy.challenge.Blog.models;
 
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "posts")
+@SQLDelete(sql = "UPDATE posts SET deleted=true WHERE id = ?")
+@Where(clause = "deleted = false")
 public class Post {
 
     @Id
@@ -25,4 +30,6 @@ public class Post {
     public Date creationDate;
 
     public Long userId;
+
+    private Boolean deleted = false;
 }
