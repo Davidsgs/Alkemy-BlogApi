4823def34f8f5bcb01e817d5382bdbbabd68877e
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-06-19 22:30:29 -0300

    Improving the code.

--- src/main/java/com/alkemy/challenge/Blog/config/SecurityConfigurer.java
+++ src/main/java/com/alkemy/challenge/Blog/config/SecurityConfigurer.java
@@ -3,6 +3,7 @@ import com.alkemy.challenge.Blog.services.MyUserDetailsService;
 import com.alkemy.challenge.Blog.util.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
